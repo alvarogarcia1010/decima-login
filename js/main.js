4abffbddf357026ab0b1bab1c8114d18948b4df9
alvarogarcia1010 <00111317@uca.edu.sv>
2020-03-05 15:33:08 -0600

    Issue #1

--- js/main.js
+++ js/main.js
@@ -1,5 +1,38 @@
 $(document).ready(function()
 {
+  $('.validate-form .form-control').each(function()
+  {
+      $(this).focus(function()
+      {
+         hideValidate(this);
+      });
+  });
+
+  function validate (input) {
+      if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+          if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+              return false;
+          }
+      }
+      else {
+          if($(input).val().trim() == ''){
+              return false;
+          }
+      }
+  }
+
+  function showValidate(input) {
+      var thisAlert = $(input).parent();
+
+      $(thisAlert).addClass('alert-validate');
+  }
+
+  function hideValidate(input) {
+      var thisAlert = $(input).parent();
+      console.log(thisAlert)
+      $(thisAlert).removeClass('alert-validate');
+  }
+
   $("#change-user").click(function(event)
   {
     event.preventDefault();
@@ -11,8 +44,6 @@ $(document).ready(function()
 
   $("#btn-next").click(function(event)
   {
-    event.preventDefault();
-
     Swal.fire({
       icon: 'error',
       title: 'Su correo y/o contraseña es incorrecto',
