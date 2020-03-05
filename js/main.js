dd492690fdf24f9e19aa7148b528047f97d462bd
alvarogarcia1010 <00111317@uca.edu.sv>
2020-03-05 14:43:31 -0600

    Add password reminder

--- js/main.js
+++ js/main.js
@@ -7,16 +7,51 @@ $(document).ready(function()
     $("#email-conteiner").removeClass("d-none");
   });
 
+  $('#change-user').tooltip();
+
   $("#btn-next").click(function(event)
   {
     event.preventDefault();
 
     Swal.fire({
       icon: 'error',
-      title: 'Oops...',
-      text: 'Something went wrong!',
+      title: 'Su correo y/o contraseña es incorrecto',
+      text: 'Vuelva a intentarlo o haga clic en "¿Olvidó su contraseña?" para restablecerla.',
       width: "25rem",
+      customClass:
+      {
+        title: 'font-size-small',
+        content: 'font-size-small-content',
+        cancelButton: 'bg-danger custom-btn-padding',
+      },
+      showConfirmButton: false,
+      showCancelButton: true,
+      cancelButtonText: "Cancelar",
+
     })
 
   });
+
+    $("#btn-send-password-reminder").click(function(event)
+    {
+      event.preventDefault();
+
+      Swal.fire({
+        icon: 'success',
+        title: 'Success',
+        text: 'Se ha enviado un correo para restablecer su cuenta.',
+        width: "25rem",
+        customClass:
+        {
+          title: 'font-size-small',
+          content: 'font-size-small-content',
+          cancelButton: 'bg-primary custom-btn-padding',
+        },
+        showConfirmButton: false,
+        showCancelButton: true,
+        cancelButtonText: "Aceptar",
+
+      })
+
+    });
 });
